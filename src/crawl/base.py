920138cb63a773dcac572bb5cf29125d499ef391
liangxiaoyong <liangxiaoyong@myhexin.com>
2023-04-08 13:00:51 +0800

    add request func

--- src/crawl/base.py
+++ src/crawl/base.py
@@ -1,2 +1,23 @@
 import requests
+from random import choice
+from src.crawl.settings import *
+
+
+class BaseRequest:
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def get(self, url, data, user_agent=False, headers=None, *args, **kwargs):
+        if user_agent:
+            headers = {
+                'User-Agent': choice(user_agent_list)
+            }
+        return requests.get(url, data, headers=headers)
+
+    def post(self, url, data, user_agent=False, headers=None, *args, **kwargs):
+        if user_agent:
+            headers = {
+                'User-Agent': choice(user_agent_list)
+            }
+        return requests.post(url, data, headers=headers)
 
---
+++ src/crawl/settings.py
@@ -0,0 +1,3 @@
+user_agent_list = [
+   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36'
+]
