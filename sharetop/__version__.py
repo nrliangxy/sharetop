800e6d3465572fee741f72bd3f2ff4d828835fe4
liangxy <378997468@qq.com>
2023-04-26 12:48:32 +0800

    update version

--- sharetop/__version__.py
+++ sharetop/__version__.py
@@ -2,10 +2,10 @@ __title__ = 'sharetop'
 __version__ = '1.0.0'
 __author__ = 'nrliangxy'
 __url__ = 'https://github.com/Micro-sheep/efinance'
-__author_email__ = 'micro-sheep@outlook.com'
+__author_email__ = 'nrliangxy@foxmail.com'
 __keywords__ = ['finance', 'quant', 'stock', 'fund', 'futures', 'share', 'data']
-__description__ = 'A finance tool to get stock,fund and futures data base on eastmoney'
+__description__ = 'Provide data related to the economic market'
 __project_urls__ = {
     'Documentation': 'https://efinance.readthedocs.io',
-    'Source': 'https://github.com/Micro-sheep/efinance',
+    'Source': 'https://github.com/nrliangxy/sharetop',
 }
